db251a15c26b24cf4876165277534b5db37da61d
eunchan96 <taegwon02@gmail.com>
2025-08-07 15:42:10 +0900

    확장 함수

--- src/main/kotlin/com/Main.kt
+++ src/main/kotlin/com/Main.kt
@@ -1,11 +1,14 @@
 package com
 
-data class Person(val name: String, val age: Int)
+fun String.greet(): String {
+    return "Hello, $this!"
+}
+
+fun String.greet2(): String {
+    return "Nice to meet you, $this!"
+}
 
 fun main() {
-    val person1 = Person("Alice", 30)
-    println(person1) // 자동 생성된 toString() 사용
-
-    val person2 = Person("Alice", 30)
-    println(person1 == person2) // 자동 생성된 equals() 사용
+    println("Alice".greet()) // 확장 함수 사용
+    println("Bob".greet2())  // 또 다른 확장 함수 사용
 }

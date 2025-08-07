8b72e0f3cfd4ef325a6c38d58af0770764f78b7d
eunchan96 <taegwon02@gmail.com>
2025-08-07 15:13:39 +0900

    null 처리 ?

--- src/main/kotlin/com/Main.kt
+++ src/main/kotlin/com/Main.kt
@@ -1,13 +1,21 @@
 package com
 
 fun main() {
-    val ages = mapOf(
-        "Alice" to 30,
-        "Bob" to 25,
-        "Charlie" to 35
-    )
+    val name: String? = null // nullable 타입
+    println(name?.length ?: "Name is null") // 안전한 호출 연산자와 엘비스 연산자 사용
 
-    for ((name, age) in ages) {
-        println("$name is $age years old.")
+    val person:Person = Person(Arm(Hand()), Arm(null))
+    person?.leftArm?.hand?.grab();     // 코틀린에서는 안전하게 함수 호출하려면 이렇게만 해도 됩니다
+}
+
+class Person (val leftArm: Arm?, rightArm: Arm?) {
+}
+
+class Arm (val hand: Hand?) {
+}
+
+class Hand {
+    fun grab() {
+        println("Hand is grabbing")
     }
 }

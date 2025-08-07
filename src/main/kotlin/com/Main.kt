8e4f04b66f4e117ab807c7f9743046b1e33a15bf
eunchan96 <taegwon02@gmail.com>
2025-08-07 15:48:24 +0900

    기본 매개변수

--- src/main/kotlin/com/Main.kt
+++ src/main/kotlin/com/Main.kt
@@ -1,14 +1,10 @@
 package com
 
-fun String.greet(): String {
-    return "Hello, $this!"
-}
-
-fun String.greet2(): String {
-    return "Nice to meet you, $this!"
+fun sayHello(name: String = "Guest") {
+    println("Hello, $name")
 }
 
 fun main() {
-    println("Alice".greet()) // 확장 함수 사용
-    println("Bob".greet2())  // 또 다른 확장 함수 사용
+    sayHello() // 기본값 사용
+    sayHello("Alice")
 }
